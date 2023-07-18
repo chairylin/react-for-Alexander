df10515b4b62eb493464f1024d098d3ea7da41da
barkoff11 <barkoff11112005@gmail.com>
2023-07-18 11:01:10 +0300

    restaurant

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -3,10 +3,14 @@ import React from "react";
 import { Reviews } from "../Reviews/Reviews";
 
 export const Restaurant = ({ restaurant }) => {
+  const rating = Math.floor(
+    restaurant.reviews.reduce((sum, review) => sum + review.rating, 0) /
+    restaurant.reviews.length
+  );
   return (
     <div>
       <h2>{restaurant.name}</h2>
-      {/* Rating */}
+      <Rating value={rating} />
       <Menu menu={restaurant.menu} />
 
       <Reviews reviews={restaurant.reviews} />
